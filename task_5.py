86e277fbba93731e5372163f795bfc391d3c2da5
LehlohonoloMopeli <lehlohonolompl@gmail.com>
2021-02-11 07:42:36 +0200

    Modified task 5

--- task_5.py
+++ task_5.py
@@ -6,13 +6,17 @@ def area_of_triangle(side1, side2, side3):
         type(output) : float
     
     """
-    if side1 > 0 and side2 > 0 and side3> 0:
-        semiparameter = (0.5) * (side1 + side2 + side3)        # semiparameter is equal to half the parameter of the triangle
+    try:
+        if side1 > 0 and side2 > 0 and side3> 0:
+            semiparameter = (0.5) * (side1 + side2 + side3)        # semiparameter is equal to half the parameter of the triangle
+            
+            area = sqrt(semiparameter * (semiparameter - side1) * (semiparameter - side2)
+                        * (semiparameter - side3))                 # Heron's formula
+            return area
+    
+        else:
+            return "Negative numbers not permitted!"
         
-        area = sqrt(semiparameter * (semiparameter - side1) * (semiparameter - side2)
-                    * (semiparameter - side3))                 # Heron's formula
-    
-        return area
-    
-    else:
-        return "Not a valid input!"
+    except Exception as error:                                      # To handle unwanted inputs such as strings
+        return "Not a Valid input!"
+
