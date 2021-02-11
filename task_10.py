fd5c5e067eccf7b1b38a7a06cd63cf1e7fed2fd3
LehlohonoloMopeli <lehlohonolompl@gmail.com>
2021-02-11 12:26:10 +0200

    Completed task 10

---
+++ task_10.py
@@ -0,0 +1,33 @@
+def common_characters(string1, string2):
+    """
+        Description: Takes two words as argumnts and returns the common leters between them.
+        type(output): str
+    
+    """
+    try:
+        commonLetters = []
+        
+        result = ""
+        
+        for letter in string1:
+            for element in string2:
+                if letter == element and letter not in commonLetters:       # Avoid repetition
+                    commonLetters.append(letter)
+                    
+        if len(commonLetters) == 0:
+            return "Common letters: " + str(None)
+        
+        elif len(commonLetters) == 1:
+            for element in commonLetters:
+                result += str(element)
+            return "Common letters: " + result
+        
+        elif len(commonLetters) > 1:
+            result += str(commonLetters[0])
+            for element in commonLetters[1 : ]:
+                result += ", " + str(element)
+            return "Common letters: " + result
+        
+    except Exception as error:                          # To handle unwanted input (handle all possible scenarios)
+        return "Invalid input!"
+    
